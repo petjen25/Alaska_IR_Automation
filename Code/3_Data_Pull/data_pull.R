6730cbcd6152dfa411818b3da48bf656cdcfe1f1
ferribyh <ainsleyhf@gmail.com>
2024-01-09 11:22:42 -0500

    Full water years

--- Code/3_Data_Pull/data_pull.R
+++ Code/3_Data_Pull/data_pull.R
@@ -45,20 +45,66 @@ ocean_data_pull <- TADA_BigDataRetrieval(startDate = startDate,
 
 
 #Stream
-stream_data_pull <- TADA_BigDataRetrieval(startDate = startDate,
-                                          endDate = endDate,
+# stream_data_pull <- TADA_BigDataRetrieval(startDate = startDate,
+#                                           endDate = endDate,
+#                                           siteType = 'Stream',
+#                                           statecode = 'AK',
+#                                           applyautoclean = T)
+
+stream_data_pull1 <- TADA_BigDataRetrieval(startDate = '2017-10-01',
+                                          endDate = '2018-09-30',
                                           siteType = 'Stream',
                                           statecode = 'AK',
                                           applyautoclean = T)
 
+stream_data_pull2 <- TADA_BigDataRetrieval(startDate = '2018-10-01',
+                                           endDate = '2019-09-30',
+                                           siteType = 'Stream',
+                                           statecode = 'AK',
+                                           applyautoclean = T)
+
+stream_data_pull3 <- TADA_BigDataRetrieval(startDate = '2019-10-01',
+                                           endDate = '2020-09-30',
+                                           siteType = 'Stream',
+                                           statecode = 'AK',
+                                           applyautoclean = T)
+
+stream_data_pull4 <- TADA_BigDataRetrieval(startDate = '2020-10-01',
+                                           endDate = '2021-09-30',
+                                           siteType = 'Stream',
+                                           statecode = 'AK',
+                                           applyautoclean = T)
+
+stream_data_pull5 <- TADA_BigDataRetrieval(startDate = '2021-10-01',
+                                           endDate = '2022-09-30',
+                                           siteType = 'Stream',
+                                           statecode = 'AK',
+                                           applyautoclean = T)
+
+stream_data_pull6 <- TADA_BigDataRetrieval(startDate = '2022-10-01',
+                                           endDate = '2023-09-30',
+                                           siteType = 'Stream',
+                                           statecode = 'AK',
+                                           applyautoclean = T)
+
+stream_data_pull <- stream_data_pull1 %>%
+  rbind(stream_data_pull2) %>%
+  rbind(stream_data_pull3) %>%
+  rbind(stream_data_pull4) %>%
+  rbind(stream_data_pull5) %>%
+  rbind(stream_data_pull6)
 
 ####Combine####
-all <- aswu_data_pull %>% 
-  rbind(est_data_pull) %>%
+# all <- aswu_data_pull %>% 
+#   rbind(est_data_pull) %>%
+#   rbind(lri_data_pull) %>%
+#   rbind(ocean_data_pull) %>%
+#   rbind(stream_data_pull)
+
+all <- est_data_pull %>%
   rbind(lri_data_pull) %>%
   rbind(ocean_data_pull) %>%
   rbind(stream_data_pull)
-
 
 ####Export Data####
 write_csv(est_data_pull, 'Data/data_pull/data_pull_estuary.csv')
