4479e417b3af81b8123090e564c7bb592085671d
ferribyh <ainsleyhf@gmail.com>
2024-01-24 08:59:59 -0500

    Initial commit

---
+++ Code/7_Results/categorize_au.R
@@ -0,0 +1,43 @@
+#Assign AU categories based on exceedance values
+
+#Written by: Hannah Ferriby
+
+#Load Packages
+library(tidyverse)
+
+####Load in data####
+input_analysis <- read_csv('Output/data_analysis/final_magdurfreq_output.csv')
+
+options <- input_analysis %>%
+  select(Exceed) %>%
+  unique()
+
+
+
+
+summarizeAU <- function(input_analysis){ 
+  
+  calc_individual <- input_analysis %>%
+    mutate(Individual_Category = case_when(Data_Sufficient == "(?i)No" ~ '3',
+                                           Exceed == 'Yes' ~ '5',
+                                           Exceed == 'No' ~ '2',
+                                           Exceed == 'Insufficient hardness' ~ '3',
+                                           Exceed == 'Insufficient dependent data' ~ '3',
+                                           T ~ NA))
+  
+  calc_overall <- calc_individual %>%
+    group_by(AUID_ATTNS, Use) %>%
+    mutate(cat_5_present = length(Individual_Category[Individual_Category=='5']),
+           cat_2_present = length(Individual_Category[Individual_Category=='2']),
+           Overall_Category = case_when(cat_5_present > 0 ~ '5',
+                                        cat_5_present == 0 & cat_2_present > 0 ~ '2',
+                                        cat_5_present == 0 & cat_2_present == 0 ~ '3',
+                                        T~NA)) %>%
+    select(!c(cat_5_present, cat_2_present)) %>%
+    arrange(AUID_ATTNS, Use)
+  
+  return(calc_overall)
+  
+}
+
+output <- summarizeAU(input_analysis)
