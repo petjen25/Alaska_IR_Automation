4090766b4d09490eb748362ddb486c1a1b938275
ferribyh <ainsleyhf@gmail.com>
2024-01-24 13:39:18 -0500

    Fixed magnitude output

--- Code/7_Results/summary_au_table.R
+++ Code/7_Results/summary_au_table.R
@@ -8,19 +8,34 @@ library(tidyverse)
 
 #Test data
 categorize_output <- read_csv('Output/results/categorized_aus.csv')
-# wqs_table <- read_csv('Data/data_analysis/AK_WQS_Crosswalk_20240117.csv')
+wqs_table <- read_csv('Data/data_analysis/AK_WQS_Crosswalk_20240117.csv')
 
-summarize_AU <- function(categorize_output, AU_ID) {
+summarize_AU <- function(categorize_output, wqs_table, AU_ID) {
+  
+  pull_magnitude <- wqs_table %>%
+    dplyr::select(`Waterbody Type`, `Constituent Group`,
+           Use, Constituent, Fraction, 
+           Magnitude_Text, Units, Directionality, Duration, Details)
   
   filt_data <- categorize_output %>%
     dplyr::filter(AUID_ATTNS == AU_ID) %>%
+    dplyr::select(!Magnitude_Numeric) %>%
     dplyr::select(AUID_ATTNS, `Waterbody Type`, `Constituent Group`,
                   Use, Constituent, TADA.CharacteristicName, Fraction,
                   n_Samples, n_SampDates, n_WaterYears, Data_Sufficient,
-                  Magnitude_Numeric, Units, Directionality, Duration, Details,
+                  Units, Directionality, Duration, Details,
                   Exceed, Individual_Category, Overall_Category) %>%
     unique()
+  
+  join_tables <- filt_data %>%
+    dplyr::left_join(pull_magnitude, by = c("Waterbody Type", "Constituent Group",
+                                            "Use", "Constituent", "Fraction", 
+                                            "Units", "Directionality", "Duration",
+                                            "Details"),
+                     relationship = "many-to-many")
+  
+  return(join_tables)
 
 }
 
-output <- summarize_AU(categorize_output, AU_ID = 'AK_B_1010203_001')
+output <- summarize_AU(categorize_output, wqs_table, AU_ID = 'AK_B_1010203_001')
