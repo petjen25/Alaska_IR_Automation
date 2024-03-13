e7638e38f8be0955767d01fe494781abd30c939a
ferribyh <ainsleyhf@gmail.com>
2024-03-13 10:45:32 -0400

    Updated with Kateri's comments

--- Code/8_Stakeholder_Communication/create_summary_pdfs.R
+++ Code/8_Stakeholder_Communication/create_summary_pdfs.R
@@ -20,13 +20,17 @@ ak <- st_read('Data/data_GIS/cb_2018_us_state_500k/cb_2018_us_state_500k.shp') %
   st_transform(crs = st_crs(au_shape_crs))
 
 
+# unique_AU <- output_df %>%
+#   select(AUID_ATTNS) %>%
+#   unique() %>%
+#   head()
+
 unique_AU <- output_df %>%
   select(AUID_ATTNS) %>%
-  unique() %>%
-  head()
+  unique()
 
 # Loop --------------------------------------------------------------------
-
+au_loop <- unique_AU$AUID_ATTNS[[2]]
 
 library(rmarkdown)  # for render
 
@@ -64,10 +68,10 @@ for (au_loop in unique_AU$AUID_ATTNS){  # for each unique episode
     print("No AU Type Detected")
   )
   
-  # Now render ('knit') the R Markdown file to Word format, name it and save it
+  # Now render ('knit') the R Markdown file to pdf format, name it and save it
   
   render(
-    input = "Code/7_Results/summary_pdfs_template.rmd",  # path to the template
+    input = "Code/8_Stakeholder_Communication/summary_pdfs_template.rmd",  # path to the template
     output_file = paste0("au_", au_loop, ".pdf"),  # name the output
     output_dir = "Output/results/summary_pdfs"  # folder in which to put the output file
   )
