7bfbdfd1bf684f837bfa2051e3f5434f205198f6
ferribyh <ainsleyhf@gmail.com>
2024-03-27 10:24:14 -0400

    fixed joins

--- Code/7_Results/create_attains_AU.R
+++ Code/7_Results/create_attains_AU.R
@@ -24,25 +24,34 @@ data_sufficiency_AUID_added <- data_sufficiency %>%
 #Join with previous AU ATTAINS by each AUID type
 data_current_AU <- data_sufficiency_AUID_added %>%
   mutate(assessmentUnitId = AUID_ATTNS) %>%
-  right_join(previous_au_attains, by = c('assessmentUnitId'))
+  inner_join(previous_au_attains, by = c('assessmentUnitId'))
 
 #Find retired AUs
 data_retired_AU <- data_sufficiency_AUID_added %>%
   mutate(assessmentUnitId = Retired_AUID) %>%
-  right_join(previous_au_attains, by = c('assessmentUnitId'))
+  inner_join(previous_au_attains, by = c('assessmentUnitId'))
 
 #Find historical AUs
 data_historical_AU <- data_sufficiency_AUID_added %>%
   mutate(assessmentUnitId = Historical_AUID) %>%
-  right_join(previous_au_attains, by = c('assessmentUnitId'))
+  inner_join(previous_au_attains, by = c('assessmentUnitId'))
 
 #Combine all together
 #This becomes the starting point for each csv export process
-data_all_AUs <- data_current_AU %>%
+data_all_AUs_listed <- data_current_AU %>%
   rbind(data_retired_AU) %>%
   rbind(data_historical_AU)
 
+#Find AUs not in previous ATTAINS
+data_current_AU_not_listed <- data_sufficiency_AUID_added %>%
+  mutate(assessmentUnitId = AUID_ATTNS) %>%
+  full_join(previous_au_attains, by = c('assessmentUnitId')) %>%
+  filter(!assessmentUnitId %in% data_current_AU$assessmentUnitId) %>%
+  filter(!assessmentUnitId %in% data_retired_AU$assessmentUnitId) %>%
+  filter(!assessmentUnitId %in% data_historical_AU$assessmentUnitId)
 
+data_all_AUs <- data_all_AUs_listed %>% 
+  rbind(data_current_AU_not_listed)
 
 ####Assessment Units####
 assessment_units <- data_all_AUs %>%
