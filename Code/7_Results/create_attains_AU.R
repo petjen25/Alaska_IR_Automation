618c920a7251b32e46a4e2b520dbb94d692a1f6b
ferribyh <ainsleyhf@gmail.com>
2024-05-14 14:57:37 -0400

    Updated with AK comments

--- Code/7_Results/create_attains_AU.R
+++ Code/7_Results/create_attains_AU.R
@@ -3,7 +3,7 @@
 
 
 #Written by Hannah Ferriby
-#Date updated: 4/8/2024
+#Date updated: 5/14/2024
 
 
 ####Load Packages####
@@ -13,31 +13,39 @@ library(sf)
 library(stringi)
 
 ####Load Data####
-data_sufficiency <- read_csv('Output/data_processing/WQ_metadata_trimmed_with_data_sufficiency_20240117.csv')
-# au_id_crosswalk <- read_csv('Data/data_analysis/AUID_crosswalk.csv')
+data_sufficiency <- read_csv('Output/data_processing/WQ_metadata_trimmed_with_data_sufficiency_20240509.csv')
 previous_au_attains <- read_xlsx('Data/data_analysis/ATTAINS_AK_AsessmentUnits_DataDownload_20240126.xlsx', sheet = 2)
-samples <- read_csv('Output/data_processing/WQ_data_trimmed_long_withAU20240117.csv')
+samples <- read_csv('Output/data_processing/WQ_data_trimmed_long_withAU20240509.csv')
 ml_au_crosswalk <- read_csv('Data/data_processing/ML_AU_Crosswalk.csv')
 
 lake_aus <- st_read('Data/data_GIS/AU_Shapefiles_Corrected_20240328/lakes.shp') %>%
-  select(AUID_ATTNS, Name_AU, HUC10_ID, Shape_Leng, Shape_Area) %>%
-  st_zm() 
+  select(AUID_ATTNS, Name_AU, HUC10_ID, AU_Area) %>%
+  st_zm() %>%
+  mutate(Shape_4_Summary = AU_Area,
+         AU_Shape_Unit = 'acres') %>%
+  select(!AU_Area)
 
 river_aus <- st_read('Data/data_GIS/AU_Shapefiles_Corrected_20240328/rivers.shp') %>%
-  select(AUID_ATTNS, Name_AU, HUC10_ID, Shape_Leng, AU_Miles) %>%
-  rename(Shape_Area = AU_Miles) %>%
-  st_zm()
+  select(AUID_ATTNS, Name_AU, HUC10_ID, AU_Miles) %>%
+  st_zm() %>%
+  mutate(Shape_4_Summary = AU_Miles,
+         AU_Shape_Unit = 'miles') %>%
+  select(!AU_Miles)
 
 marine_aus <- st_read('Data/data_GIS/AU_Shapefiles_Corrected_20240328/marine.shp') %>%
-  select(AUID_ATTNS, Name_AU, HUC10_ID, Shape_Leng, Shape_Area) %>%
+  select(AUID_ATTNS, Name_AU, HUC10_ID, AU_Area) %>%
   st_zm() %>%
-  st_transform(crs = st_crs(lake_aus)) #Marine AU is in NAD83/Alaska Albers
+  st_transform(crs = st_crs(lake_aus)) %>%
+  mutate(Shape_4_Summary = AU_Area,
+         AU_Shape_Unit = 'square miles') %>%
+  select(!AU_Area)#Marine AU is in NAD83/Alaska Albers
 #All other shapefiles are in WGS 84/Pseudo-Mercator
 
 beach_aus <- st_read('Data/data_GIS/AU_Shapefiles_Corrected_20240328/beaches.shp') %>%
-  select(AUID_ATTNS, Name_AU, HUC10_ID, Shape_Leng, AU_Miles) %>%
-  rename(Shape_Area = AU_Miles) %>%
-  st_zm()
+  select(AUID_ATTNS, Name_AU, HUC10_ID, AU_Miles) %>%
+  mutate(Shape_4_Summary = AU_Miles,
+         AU_Shape_Unit = 'miles') %>%
+  select(!AU_Miles)
 
 #Combine aus into one large table, add HUC10 if missing
 all_aus <- lake_aus %>%
@@ -50,47 +58,26 @@ all_aus <- lake_aus %>%
                                                                 '[0-9]_')),
                               T ~ HUC10_ID))
 
-
-# #Combine sample data with AUID crosswalk
-# data_sufficiency_AUID_added <- data_sufficiency %>%
-#   left_join(au_id_crosswalk, by = c('AUID_ATTNS' = 'Active_AUID'))
-# 
-# 
-# #Join with previous AU ATTAINS by each AUID type
-# data_current_AU <- data_sufficiency_AUID_added %>%
-#   mutate(assessmentUnitId = AUID_ATTNS) %>%
-#   inner_join(previous_au_attains, by = c('assessmentUnitId'))
-# 
-# #Find retired AUs
-# data_retired_AU <- data_sufficiency_AUID_added %>%
-#   mutate(assessmentUnitId = Retired_AUID) %>%
-#   inner_join(previous_au_attains, by = c('assessmentUnitId'))
-# 
-# #Find historical AUs
-# data_historical_AU <- data_sufficiency_AUID_added %>%
-#   mutate(assessmentUnitId = Historical_AUID) %>%
-#   inner_join(previous_au_attains, by = c('assessmentUnitId'))
-# 
-# #Combine all together
-# #This becomes the starting point for each csv export process
-# data_all_AUs_listed <- data_current_AU %>%
-#   rbind(data_retired_AU) %>%
-#   rbind(data_historical_AU)
-
 #Find AUs info not in previous ATTAINS
 data_current_AU_not_listed <- data_sufficiency %>%
   mutate(assessmentUnitId = AUID_ATTNS) %>%
   anti_join(previous_au_attains, by = c('assessmentUnitId')) %>%
   #Join with AU shapefile information
-  left_join(all_aus, by = 'AUID_ATTNS') 
+  left_join(all_aus, by = 'AUID_ATTNS') %>%
+  #Add HUC10 if missing
+  mutate(HUC10_ID = ifelse(is.na(HUC10_ID),
+                                 paste0('190', str_extract(AUID_ATTNS, "[0-9.]+")),
+                                 HUC10_ID))
 
 ####Assessment Units####
 assessment_units <- data_current_AU_not_listed %>%
   rename(ASSESSMENT_UNIT_ID = AUID_ATTNS,
-         USE_CLASS_NAME = AU_Type,
-         ASSESSMENT_UNIT_NAME = Name_AU,
-         LOCATION_DESCRIPTION = HUC10_ID) %>%
-  mutate(ASSESSMENT_UNIT_STATE = 'AK',
+         ASSESSMENT_UNIT_NAME = Name_AU) %>%
+  mutate(USE_CLASS_NAME = NA,
+         LOCATION_DESCRIPTION = paste0('Located in HUC', HUC10_ID,', ',
+                                       round(Shape_4_Summary, 3), ' ',
+                                       AU_Shape_Unit, '.'),
+         ASSESSMENT_UNIT_STATE = 'AK',
          ASSESSMENT_UNIT_AGENCY = 'S', #S for state agency
          ASSESSMENT_UNIT_COMMENT = NA) %>% #ASK ABOUT COMMENTS
   select(ASSESSMENT_UNIT_ID, ASSESSMENT_UNIT_NAME, ASSESSMENT_UNIT_STATE,
@@ -98,32 +85,28 @@ assessment_units <- data_current_AU_not_listed %>%
          USE_CLASS_NAME) %>% #Change order to reflect template
   unique() 
 
-#Some AUs have duplicates from having use class and some having NA
-duplicate_values_na <- assessment_units %>% 
-  group_by_at(vars(-USE_CLASS_NAME)) %>% #Find duplicate rows based on one having
-  #a use class and another having NA
-  filter(n() > 1) %>%
-  #filter out NA values 
-  filter(is.na(USE_CLASS_NAME))
-
-#Remove all NA duplicated values in the assessment
-combine_assessment_units <- assessment_units %>%
-  anti_join(duplicate_values_na, by = c('ASSESSMENT_UNIT_ID', 'ASSESSMENT_UNIT_NAME', 'ASSESSMENT_UNIT_STATE',
-                                        'ASSESSMENT_UNIT_AGENCY', 'ASSESSMENT_UNIT_COMMENT', 'LOCATION_DESCRIPTION',
-                                        'USE_CLASS_NAME'))
-
-write_csv(combine_assessment_units, 'Output/results/ATTAINS/AU_Batch_Upload/Assessment_Units.csv',
+write_csv(assessment_units, 'Output/results/ATTAINS/AU_Batch_Upload/Assessment_Units.csv',
           na="")
 
 ####Water Types####
 water_types <- data_current_AU_not_listed %>%
-  select(AUID_ATTNS, AU_Type, Shape_Area) %>%
+  select(AUID_ATTNS, AU_Type, Shape_4_Summary, AU_Shape_Unit, Name_AU) %>%
   rename(ASSESSMENT_UNIT_ID = AUID_ATTNS,
-         WATER_SIZE = Shape_Area) %>%
-  mutate(         WATER_TYPE = toupper(AU_Type),
-                  WATER_UNIT = case_when(AU_Type == 'RIVER' | AU_Type == 'BEACH' ~
-                                           'Miles',
-                                         T ~ 'Square Miles')) %>%
+         WATER_SIZE = Shape_4_Summary) %>%
+  mutate(WATER_TYPE = ifelse(AU_Type == 'River', 'STREAM/CREEK/RIVER', toupper(AU_Type)),
+         WATER_TYPE = case_when(AU_Type == 'MARINE' & str_detect(Name_AU, 'Harbor') ~
+                                 'HARBOR',
+                                AU_Type == 'MARINE' & str_detect(Name_AU, 'Estuary') ~
+                                  'ESTUARY',
+                                AU_Type == 'MARINE' & str_detect(Name_AU, 'Lagoon') ~
+                                  'LAGOON',
+                                AU_Type == 'MARINE' & (str_detect(Name_AU, 'Channel') |
+                                                         str_detect(Name_AU, 'Bay') )~
+                                  'CHANNEL AND BAY',
+                                AU_Type == 'MARINE' ~
+                                  'COASTAL',
+                                T ~ WATER_TYPE), 
+         WATER_UNIT = AU_Shape_Unit) %>%
   select(ASSESSMENT_UNIT_ID, WATER_TYPE, WATER_SIZE, WATER_UNIT) %>% #Reorder columns
   unique()
 
@@ -133,10 +116,10 @@ write_csv(water_types, 'Output/results/ATTAINS/AU_Batch_Upload/Water_Types.csv',
 ####Locations####
 locations <- data_current_AU_not_listed %>%
   select(AUID_ATTNS, HUC10_ID) %>%
-  rename(ASSESSMENT_UNIT_ID = AUID_ATTNS,
-         LOCATION_TYPE_CONTEXT = HUC10_ID) %>% 
+  rename(ASSESSMENT_UNIT_ID = AUID_ATTNS) %>% 
   mutate(LOCATION_TYPE_CODE = 'HUC-10',
-         LOCATION_TEXT = NA) %>% #To be filled in manually be AK DEC
+         LOCATION_TYPE_CONTEXT = 'AKDECWQ',
+         LOCATION_TEXT = paste0('HUC',HUC10_ID)) %>% #Add 190
   select(ASSESSMENT_UNIT_ID, LOCATION_TYPE_CODE, LOCATION_TYPE_CONTEXT, 
          LOCATION_TEXT) %>%
   unique()
@@ -145,7 +128,7 @@ write_csv(locations, 'Output/results/ATTAINS/AU_Batch_Upload/Locations.csv',
           na="")
 
 ####Monitoring Stations####
-ml_names <- combine_assessment_units %>%
+ml_names <- assessment_units %>%
   left_join(ml_au_crosswalk, by = c('ASSESSMENT_UNIT_ID' = 'AUID_ATTNS'))
 
 monitoring_stations <- ml_names %>%
