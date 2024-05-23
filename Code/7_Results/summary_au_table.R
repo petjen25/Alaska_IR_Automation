0d8b27abfbe2b449ff8aaca261f52d7f34f9edda
ferribyh <ainsleyhf@gmail.com>
2024-05-23 11:41:55 -0400

    Update methods to create summary table - update inputs

--- Code/7_Results/summary_au_table.R
+++ Code/7_Results/summary_au_table.R
@@ -5,9 +5,11 @@
 
 #Required libraries
 library(tidyverse)
+library(readxl)
+library(sf)
 
 #Test data
-categorize_output <- read_csv('Output/results/categorized_aus_20240222.csv')
+categorize_output <- read_csv('Output/results/categorized_aus_20240515.csv')
 au_id_crosswalk <- read_csv('Data/data_analysis/AUID_crosswalk.csv')
 previous_au_attains <- read_xlsx('Data/data_analysis/ATTAINS_AK_AsessmentUnits_DataDownload_20240126.xlsx', sheet = 2)
 
@@ -16,44 +18,44 @@ table_cat <- categorize_output %>%
   select(AUID_ATTNS, `Waterbody Type`, Use, Use_Category) %>%
   unique()
 
-#Combine sample data with AUID crosswalk
-data_cat_AUID_added <- table_cat %>%
-  left_join(au_id_crosswalk, by = c('AUID_ATTNS' = 'Active_AUID'))
+lake_aus <- st_read('Data/data_GIS/AU_Shapefiles_Corrected_20240328/lakes.shp') %>%
+  select(AUID_ATTNS, Name_AU, HUC10_ID, AU_Area) %>%
+  st_zm() %>%
+  mutate(Shape_4_Summary = AU_Area,
+         AU_Shape_Unit = 'acres') %>%
+  select(!AU_Area)
 
+river_aus <- st_read('Data/data_GIS/AU_Shapefiles_Corrected_20240328/rivers.shp') %>%
+  select(AUID_ATTNS, Name_AU, HUC10_ID, AU_Miles) %>%
+  st_zm() %>%
+  mutate(Shape_4_Summary = AU_Miles,
+         AU_Shape_Unit = 'miles') %>%
+  select(!AU_Miles)
 
-#Join with previous AU ATTAINS by each AUID type
-data_current_AU <- data_cat_AUID_added %>%
-  mutate(assessmentUnitId = AUID_ATTNS) %>%
-  inner_join(previous_au_attains, by = c('assessmentUnitId'))
+marine_aus <- st_read('Data/data_GIS/AU_Shapefiles_Corrected_20240328/marine.shp') %>%
+  select(AUID_ATTNS, Name_AU, HUC10_ID, AU_Area) %>%
+  st_zm() %>%
+  st_transform(crs = st_crs(lake_aus)) %>%
+  mutate(Shape_4_Summary = AU_Area,
+         AU_Shape_Unit = 'square miles') %>%
+  select(!AU_Area)#Marine AU is in NAD83/Alaska Albers
+#All other shapefiles are in WGS 84/Pseudo-Mercator
 
-#Find retired AUs
-data_retired_AU <- data_cat_AUID_added %>%
-  mutate(assessmentUnitId = Retired_AUID) %>%
-  inner_join(previous_au_attains, by = c('assessmentUnitId'))
+beach_aus <- st_read('Data/data_GIS/AU_Shapefiles_Corrected_20240328/beaches.shp') %>%
+  select(AUID_ATTNS, Name_AU, HUC10_ID, AU_Miles) %>%
+  mutate(Shape_4_Summary = AU_Miles,
+         AU_Shape_Unit = 'miles') %>%
+  select(!AU_Miles)
 
-#Find historical AUs
-data_historical_AU <- data_cat_AUID_added %>%
-  mutate(assessmentUnitId = Historical_AUID) %>%
-  inner_join(previous_au_attains, by = c('assessmentUnitId'))
+#Combine aus into one large table, add HUC10 if missing
+all_aus <- lake_aus %>%
+  rbind(river_aus) %>%
+  rbind(marine_aus) %>%
+  rbind(beach_aus) 
 
-#Combine all together
-data_all_AUs_listed <- data_current_AU %>%
-  rbind(data_retired_AU) %>%
-  rbind(data_historical_AU)
-
-#Find AUs not in previous ATTAINS
-data_current_AU_not_listed <- data_cat_AUID_added %>%
-  mutate(assessmentUnitId = AUID_ATTNS) %>%
-  full_join(previous_au_attains, by = c('assessmentUnitId')) %>%
-  filter(!assessmentUnitId %in% data_current_AU$assessmentUnitId) %>%
-  filter(!assessmentUnitId %in% data_retired_AU$assessmentUnitId) %>%
-  filter(!assessmentUnitId %in% data_historical_AU$assessmentUnitId)
-
-#Combine all AU information together
-#Create new column overallStatus that is just the string version of state
-#numerical categories
-data_all_AUs <- data_all_AUs_listed %>% 
-  rbind(data_current_AU_not_listed) 
+#Join with AU shapefile information
+data_all_AUs <- table_cat %>% 
+  left_join(all_aus, by = 'AUID_ATTNS') 
 
 #Summarize data
 final_summary <- data_all_AUs %>%
@@ -62,7 +64,7 @@ final_summary <- data_all_AUs %>%
                                    Use_Category == 5 ~ 'Not Supporting',
                                    T ~ NA)) %>% 
   select(AUID_ATTNS, `Waterbody Type`, Use, Use_Category, overallStatus, 
-         assessmentUnitName, locationDescription, waterSize, waterSizeUnits) %>%
+         Name_AU, Shape_4_Summary, AU_Shape_Unit) %>%
   unique() 
 
-write_csv(data_all_AUs, 'Output/results/summary_au_tables_20240320.csv')
+write_csv(final_summary, 'Output/results/summary_au_tables_20240523.csv')
