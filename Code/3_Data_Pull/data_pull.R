2b0fa061a32060e6ba689d45f86b84a91b329c85
ferribyh <ainsleyhf@gmail.com>
2025-04-29 16:47:22 -0400

    Update with new EPATADA version syntax

--- Code/3_Data_Pull/data_pull.R
+++ Code/3_Data_Pull/data_pull.R
@@ -2,7 +2,7 @@
 
 ##Written by: Hannah Ferriby
 ##Date created: 9-22-2023
-##Date updated: 6-28-2024
+##Date updated: 4-29-2025
 
 ####Set Up####
 library(EPATADA)
@@ -14,91 +14,42 @@ endDate <- '2023-09-30'
 ####Data Download####
 #Download by water body type
 #Aggregate surface-water-use
-aswu_data_pull <- TADA_BigDataRetrieval(startDate = startDate,
-                                        endDate = endDate,
-                                        siteType = 'Aggregate surface-water-use',
-                                        statecode = 'AK')
+aswu_data_pull <- TADA_DataRetrieval(startDate = startDate,
+                                     endDate = endDate,
+                                     siteType = 'Aggregate surface-water-use',
+                                     statecode = 'AK',
+                                     applyautoclean = T)
 
 #Estuary
-est_data_pull <- TADA_BigDataRetrieval(startDate = startDate,
-                                       endDate = endDate,
-                                       siteType = 'Estuary',
-                                       statecode = 'AK',
-                                       applyautoclean = T)
+est_data_pull <- TADA_DataRetrieval(startDate = startDate,
+                                    endDate = endDate,
+                                    siteType = 'Estuary',
+                                    statecode = 'AK',
+                                    applyautoclean = T)
 
 
 #Lake, Reservoir, Impoundment
-lri_data_pull <- TADA_BigDataRetrieval(startDate = startDate,
-                                       endDate = endDate,
-                                       siteType = 'Lake, Reservoir, Impoundment',
-                                       statecode = 'AK',
-                                       applyautoclean = T)
+lri_data_pull <- TADA_DataRetrieval(startDate = startDate,
+                                    endDate = endDate,
+                                    siteType = 'Lake, Reservoir, Impoundment',
+                                    statecode = 'AK',
+                                    applyautoclean = T)
 
 
 #Ocean
-ocean_data_pull <- TADA_BigDataRetrieval(startDate = startDate,
-                                         endDate = endDate,
-                                         siteType = 'Ocean',
-                                         statecode = 'AK',
-                                         applyautoclean = T)
+ocean_data_pull <- TADA_DataRetrieval(startDate = startDate,
+                                      endDate = endDate,
+                                      siteType = 'Ocean',
+                                      statecode = 'AK',
+                                      applyautoclean = T)
 
 
 #Stream
-stream_data_pull <- TADA_BigDataRetrieval(startDate = startDate,
-                                          endDate = endDate,
-                                          siteType = 'Stream',
-                                          statecode = 'AK',
-                                          applyautoclean = T)
-
-# stream_data_pull1 <- TADA_BigDataRetrieval(startDate = '2017-10-01',
-#                                           endDate = '2018-09-30',
-#                                           siteType = 'Stream',
-#                                           statecode = 'AK',
-#                                           applyautoclean = T)
-# 
-# stream_data_pull2 <- TADA_BigDataRetrieval(startDate = '2018-10-01',
-#                                            endDate = '2019-09-30',
-#                                            siteType = 'Stream',
-#                                            statecode = 'AK',
-#                                            applyautoclean = T)
-# 
-# stream_data_pull3 <- TADA_BigDataRetrieval(startDate = '2019-10-01',
-#                                            endDate = '2020-09-30',
-#                                            siteType = 'Stream',
-#                                            statecode = 'AK',
-#                                            applyautoclean = T)
-# 
-# stream_data_pull4 <- TADA_BigDataRetrieval(startDate = '2020-10-01',
-#                                            endDate = '2021-09-30',
-#                                            siteType = 'Stream',
-#                                            statecode = 'AK',
-#                                            applyautoclean = T)
-# 
-# stream_data_pull5 <- TADA_BigDataRetrieval(startDate = '2021-10-01',
-#                                            endDate = '2022-09-30',
-#                                            siteType = 'Stream',
-#                                            statecode = 'AK',
-#                                            applyautoclean = T)
-# 
-# stream_data_pull6 <- TADA_BigDataRetrieval(startDate = '2022-10-01',
-#                                            endDate = '2023-09-30',
-#                                            siteType = 'Stream',
-#                                            statecode = 'AK',
-#                                            applyautoclean = T)
-# 
-# stream_data_pull <- stream_data_pull1 %>%
-#   rbind(stream_data_pull2) %>%
-#   rbind(stream_data_pull3) %>%
-#   rbind(stream_data_pull4) %>%
-#   rbind(stream_data_pull5) %>%
-#   rbind(stream_data_pull6)
-
-####Combine####
-# all <- aswu_data_pull %>% 
-#   rbind(est_data_pull) %>%
-#   rbind(lri_data_pull) %>%
-#   rbind(ocean_data_pull) %>%
-#   rbind(stream_data_pull)
+stream_data_pull <- TADA_DataRetrieval(startDate = startDate,
+                                       endDate = endDate,
+                                       siteType = 'Stream',
+                                       statecode = 'AK',
+                                       applyautoclean = T)
 
 all <- est_data_pull %>%
   rbind(lri_data_pull) %>%
